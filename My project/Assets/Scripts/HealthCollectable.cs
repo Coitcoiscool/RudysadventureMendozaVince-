b0a5ab396e21a554962696bec763790a7a08151b
Coitcoiscool <145067114+Coitcoiscool@users.noreply.github.com>
2023-11-15 13:49:00 -0800

    Health obj
    am half way though the vid

---
+++ My project/Assets/Scripts/HealthCollectable.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthCollectable : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("object that entered the trigger is:" + other);
+    }
+}
