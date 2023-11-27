9cb2878e47da14047ebc22fca0d3f9674854410f
Coitcoiscool <145067114+Coitcoiscool@users.noreply.github.com>
2023-11-26 20:56:43 -0800

    added the bot to move a make the player take dmg

--- My project/Assets/Scripts/RubyController.cs
+++ My project/Assets/Scripts/RubyController.cs
@@ -6,9 +6,15 @@ using UnityEngine;
 public class RubyController : MonoBehaviour
 {
     public float speed = 3.0f;
+
     public int maxHealth = 5;
+    public float timeInvincible = 2;
     public int health { get { return currentHealth; } }
     int currentHealth;
+
+
+    bool isInvincible;
+    float invincibleTimer;
 
 
     Rigidbody2D rigidbody2d;
@@ -26,8 +32,17 @@ public class RubyController : MonoBehaviour
     {
          horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if(invincibleTimer < 0)
+            {
+                isInvincible = false;
+            }
+        }
     }
-    private void FixedUpdate()
+     void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
@@ -38,6 +53,15 @@ public class RubyController : MonoBehaviour
     }
     public void ChangeHealth(int amount)
     {
+        if(amount < 0) 
+        {
+             if(isInvincible) 
+             {
+                return;
+             }
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
